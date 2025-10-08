c35896e24fc16f25e307d34d717022930d864072
Zipeng Deng <zd2513a@american.edu>
2025-10-07 20:40:41 -0400

    05

---
+++ 05-external-libraries/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  Toastify({
+    text: "Welcome 'Take a break coffee!' Today, the second cup of cappuccino is half price ☕",
+    duration: 3000,
+    gravity: "top",
+    position: "center",
+    close: true
+  }).showToast();
+
+  const btn = document.getElementById("dealBtn");
+  const synth = new Tone.Synth().toDestination();
+
+  btn.addEventListener("click", () => {
+    synth.triggerAttackRelease("A3", "16n");
+
+    Toastify({
+      text: "Today's Special: Buy two cups of caramel latte and get the third one free!",
+      duration: 3000,
+      gravity: "top",
+      position: "center",
+      close: true
+    }).showToast();
+  });
+});
