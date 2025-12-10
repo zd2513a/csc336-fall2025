3f2af27036b2aac9c54a1c009ae8ee1898ae799a
Zipeng Deng <zd2513a@american.edu>
2025-12-10 00:36:15 -0500

    08-server-world

--- 08-server-world/server.js
+++ 08-server-world/server.js
@@ -1,40 +1,57 @@
 import express from "express";
-import fs from "fs";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static("./public"));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
+
+const worldPath = path.join(__dirname, "world.json");
+
+// Web Page
+app.get("/", (req, res) => {
+  console.log("GET /");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Get world.json
 app.get("/world", async (req, res) => {
-    const dataString = await fs.readFileSync("world.json", "utf-8");
-    const dataObject = JSON.parse(dataString);
-    res.json(dataObject);
+  console.log("GET /world");
+  const data = await fs.readFile(worldPath, "utf-8");
+  res.json(JSON.parse(data));
 });
 
+// Update world
 app.post("/update", async (req, res) => {
-    const worldData = await fs.readFileSync("./world.json", "utf-8");
-    const world = JSON.parse(worldData);
+  console.log("POST /update", req.body);
 
-    const AddRegion = req.body;
-    
-    for (let region of world.regions) {
-        for (let city of region.towns) {
-            for (let person of city.notable_people) {
-                if (person.name === AddRegion.region) {
-                    person.name += "!!!";
-                }
-            }
-        }
-    }
+  const { regionName, townName, personName, personRole } = req.body;
 
-    // Write it back to file
-    await fs.writeFileSync("world.json", JSON.stringify(world, null, 2));
+  const world = JSON.parse(await fs.readFile(worldPath, "utf-8"));
 
-    // Now that we've modified the world data, and written it back to file
-    // send it back to the client.
-    res.json(world);
+  const region = world.regions.find(r => r.name === regionName);
+  const town = region.towns.find(t => t.name === townName);
+
+  town.notable_people.push({
+    name: personName,
+    role: personRole,
+    items: []
+  });
+
+  await fs.writeFile(worldPath, JSON.stringify(world, null, 2));
+  res.json(world);
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3001"));
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
